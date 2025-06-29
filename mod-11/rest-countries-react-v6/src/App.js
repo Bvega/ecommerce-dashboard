25eb0daa2e68bcde12ddec23ec6287971d6c23c9
Bvega <bolivar.vega@gmail.com>
2025-06-28 21:49:53 -0400

    TailwindCSS setup working with Vite + React + TS after clean reinstall

---
+++ mod-11/rest-countries-react-v6/src/App.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function App() {
+    return (<div className="text-center p-10 bg-gray-100 dark:bg-gray-800 min-h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">TailwindCSS is working!</h1>
+    </div>);
+}
+exports.default = App;
---
+++ mod-11/rest-countries-react-v6/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center p-10 bg-gray-100 dark:bg-gray-800 min-h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">TailwindCSS is working!</h1>
+    </div>
+  );
+}
+
+export default App;
