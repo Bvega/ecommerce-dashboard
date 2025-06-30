3e1526dd5b5d53481c15c30118df0191012257d1
Bvega <bolivar.vega@gmail.com>
2025-06-29 22:58:26 -0400

    Backup: current working React REST Countries app before Tailwind integration

--- mod-11/client/src/components/BorderLink.jsx
+++ mod-11/client/src/components/BorderLink.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const BorderLink = ({ code }) => {
-  return (
-    <Link
-      to={`/country/${code}`}
-      className="px-3 py-1 shadow rounded hover:opacity-80 transition"
-    >
-      {code}
-    </Link>
-  );
-};
+const BorderLink = ({ code }) => (
+  <Link
+    to={`/country/${code}`}
+    className="px-4 py-2 bg-var(--element-bg) text-var(--text-color) rounded shadow hover:bg-opacity-80 transition"
+  >
+    {code}
+  </Link>
+);
 
 export default BorderLink;
